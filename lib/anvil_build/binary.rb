0e182de4d8b6dd0e0160402662f088e0adb50c29
Terence Lee <hone02@gmail.com>
2012-12-20 20:50:36 -0500

    pass build dir to block, to make the contract clearer

--- lib/anvil_build/binary.rb
+++ lib/anvil_build/binary.rb
@@ -17,14 +17,13 @@ module AnvilBuild
 
     # this is the main compile method.
     # pass a block to compile of what you want to run.
-    # the block gets the build dir, which is what you want
-    # to either use as the --prefix for most configure blocks
-    # or at least ensure the artifacts you want to be in the
-    # final tarball is there.
+    # the block gets the source dir to work in, and a
+    # build dir. An output tarball is created from the contents
+    # of this final tarball.
     def compile
       tmpdir = Dir.mktmpdir
-      Dir.chdir(tmpdir) do |dir|
-        yield dir
+      Dir.chdir(tmpdir) do |source_dir, build_dir|
+        yield source_dir, @build_dir
       end
 
       puts "Packaging the following files/dirs:"
