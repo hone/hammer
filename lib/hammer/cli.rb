757d2be5908498b9b37e63e0785d9d817f73e760
Terence Lee <hone02@gmail.com>
2013-01-07 16:40:38 -0800

    remove debug/local stuff for now

--- lib/hammer/cli.rb
+++ lib/hammer/cli.rb
@@ -31,87 +31,43 @@ module Hammer
     end
 
     desc "build", "builds the binary"
-    method_option :local, :type => :boolean, :default => false,
-      :desc => "flag to do a local build"
-    method_option :debug, :type => :boolean, :default => false,
-      :desc => "set build to be debugged"
     method_option :build, :type => :string, :default => ".",
       :desc => "path to the build scripts"
     method_option :env, :type => :hash,
       :desc => "build environment to pass to the build script"
     def build
-      env = {:DEBUG => options[:debug]}
-      cmd = ""
-      if env.values.any?
-        cmd << "env "
-        env.each do |key, value|
-          cmd << "#{key}=#{value}" if value
-        end
-        cmd << " "
-      end
+      require 'anvil/engine'
+      slug_url = nil
+      pwd      = Dir.pwd
 
-      if options[:local]
-        tmpdir    = Dir.mktmpdir
-        build_dir = "#{tmpdir}/build"
-        cache_dir = "#{tmpdir}/cache"
-        FileUtils.mkdir_p build_dir
-        FileUtils.mkdir_p cache_dir
+      Dir.mktmpdir do |dir|
+        system("cp -rf #{options[:build]}/* #{dir}")
 
-        puts "Building..."
-        cmd << "bin/compile #{build_dir} #{cache_dir}"
-        pipe cmd
-        puts "Done."
+        Dir.chdir(dir) do
+          FileUtils.cp_r(File.join(vendor_dir, "bin"), ".")
+          FileUtils.cp_r(File.join(vendor_dir, "bundle"), ".")
+          FileUtils.cp(File.join(vendor_dir, "Gemfile"), ".")
+          FileUtils.cp(File.join(vendor_dir, "Gemfile.lock"), ".")
 
-        require 'digest/sha1'
-        package_name = Digest::SHA1.hexdigest(rand.to_s)
-        Dir.chdir(build_dir) do
-          puts "Packaging..."
-          pipe "tar cvf ../#{package_name}.tgz ."
-        end
-        FileUtils.mv("#{tmpdir}/#{package_name}.tgz", ".")
-        puts "tarball here: ./#{package_name}.tgz"
-      else
-        require 'anvil/engine'
-        slug_url = nil
-        pwd      = Dir.pwd
-
-        Dir.mktmpdir do |dir|
-          system("cp -rf #{options[:build]}/* #{dir}")
-
-          Dir.chdir(dir) do
-            FileUtils.cp_r(File.join(vendor_dir, "bin"), ".")
-            FileUtils.cp_r(File.join(vendor_dir, "bundle"), ".")
-            FileUtils.cp(File.join(vendor_dir, "Gemfile"), ".")
-            FileUtils.cp(File.join(vendor_dir, "Gemfile.lock"), ".")
-
-            if options[:env]
-              File.open('env', 'wb') do |file|
-                options[:env].each {|k, v| file.puts "#{k}=#{v}" }
-              end
+          if options[:env]
+            File.open('env', 'wb') do |file|
+              options[:env].each {|k, v| file.puts "#{k}=#{v}" }
             end
-
-            slug_url = Anvil::Engine.build(".", :buildpack => ".")
           end
-        end
 
-        Dir.mktmpdir do |dir|
-          filename = URI.parse(slug_url).path.sub("/slugs/", "")
-
-          Dir.chdir(dir) do
-            system("curl -O #{slug_url}")
-            system("tar zxf #{filename}")
-            system("rm -rf #{filename} .profile.d .bundle Procfile .gitignore")
-            system("tar czf #{filename} *")
-            system("mv #{filename} #{pwd}")
-          end
+          slug_url = Anvil::Engine.build(".", :buildpack => ".")
         end
       end
-    ensure
-      if options[:local]
-        if env[:DEBUG]
-          puts "Build artifacts here: #{tmpdir}"
-        else
-          FileUtils.rm_rf(tmpdir)
+
+      Dir.mktmpdir do |dir|
+        filename = URI.parse(slug_url).path.sub("/slugs/", "")
+
+        Dir.chdir(dir) do
+          system("curl -O #{slug_url}")
+          system("tar zxf #{filename}")
+          system("rm -rf #{filename} .profile.d .bundle Procfile .gitignore")
+          system("tar czf #{filename} *")
+          system("mv #{filename} #{pwd}")
         end
       end
     end
